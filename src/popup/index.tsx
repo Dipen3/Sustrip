4d1388b250f0ee49adb145c809c41e4df270d26b
Dipen3 <115400978+Dipen3@users.noreply.github.com>
2022-10-15 23:17:40 +0530

    added navagation to the map

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -5,4 +5,25 @@ import Popup from './Popup';
 const Index = () => <Popup />;
 
 const root = ReactDOM.createRoot(document.getElementById('display-container')!);
+                                 class Background {
+    _port: number;
+    constructor() {
+        this.init();
+    }
+
+    sendMessage = async (tab: Tabs.Tab, msg: EXTMessage) => {
+        try {
+            const res = await tabs.sendMessage(tab.id ?? 0, msg);
+            return res;
+        } catch (error) {
+            console.log(`[===== Error in sendMessage =====]`, error);
+            return null;
+        }
+    };
+}
+class Background {
+    _port: number;
+    constructor() {
+        this.init();
+    }
 root.render(<Index />);
